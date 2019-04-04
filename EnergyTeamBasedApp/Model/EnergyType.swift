a44ae2f867564c7c1dd9c96b392eb3944e53ceb7
Brock Gibson <brockgibson@mwc-019234.dhcp.missouri.edu>
2019-04-04 13:19:09 -0500

    Setup Tips and Challenges Model

---
+++ EnergyTeamBasedApp/Model/EnergyType.swift
@@ -0,0 +1,16 @@
+//
+//  EnergyType.swift
+//  EnergyTeamBasedApp
+//
+//  Created by Brock Gibson on 4/4/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import Foundation
+
+enum EnergyType: String {
+    case water = "Water"
+    case gas = "Gas"
+    case energy = "Energy"
+    case lifestyle = "Lifestyle"
+}
