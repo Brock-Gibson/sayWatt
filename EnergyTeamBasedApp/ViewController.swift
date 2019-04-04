70945a427bebe782ccf5a7b395a63595255175e3
Brock Gibson <brockgibson@mwc-019234.dhcp.missouri.edu>
2019-04-04 12:42:57 -0500

    First Commit

---
+++ EnergyTeamBasedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnergyTeamBasedApp
+//
+//  Created by Brock Gibson on 4/4/19.
+//  Copyright © 2019 Brock Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
