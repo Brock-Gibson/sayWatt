ddaac13df3099c739a77b989ebcccd685c5b37b8
Brock Gibson <brockgibson@mwc-017173.dhcp.missouri.edu>
2019-04-11 13:26:06 -0500

    update core data model

--- EnergyTeamBasedApp/Model/EnergyBill+CoreDataClass.swift
+++ EnergyTeamBasedApp/Model/EnergyBill+CoreDataClass.swift
@@ -9,8 +9,30 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 @objc(EnergyBill)
 public class EnergyBill: NSManagedObject {
-
+    
+    var date: Date? {
+        get {
+            return billDate as Date?
+        }
+        set {
+            billDate = newValue as NSDate?
+        }
+    }
+    
+    convenience init?(amount: Double, date: Date, desc: String?) {
+        let appDelegate  = UIApplication.shared.delegate as? AppDelegate
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        
+        self.init(entity: EnergyBill.entity(), insertInto: managedContext)
+        
+        self.desc = desc
+        self.amount = Double(amount)
+        self.date = date
+    }
 }
--- EnergyTeamBasedApp/Model/Profile+CoreDataClass.swift
+++ EnergyTeamBasedApp/Model/Profile+CoreDataClass.swift
@@ -9,8 +9,47 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 @objc(Profile)
 public class Profile: NSManagedObject {
-
+    var EnergyBills: [EnergyBill]? {
+        return self.rawEnergyBills?.array as? [EnergyBill]
+    }
+    
+    var date: Date? {
+        get {
+            return birthday as Date?
+        }
+        set {
+            birthday = newValue as NSDate?
+        }
+    }
+    
+    var image: UIImage? {
+        get {
+            if let imageData = profilePic as Data? {
+                return UIImage(data: imageData)
+            }
+            return nil
+        }
+        set {
+            let imageData = newValue!.pngData() as NSData?
+            profilePic = imageData
+        }
+    }
+    
+    convenience init?(firstName: String, lastName: String, birthday: Date, image: UIImage, email: String) {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            return nil
+        }
+        
+        self.init(entity: Profile.entity(), insertInto: context)
+        
+        self.firstName = firstName
+        self.date = birthday
+        self.lastName = lastName
+        self.image = image
+    }
 }
